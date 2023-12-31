3488aef96382ae763e7d8d647c5dec4e882328ed
Shoyo Kumagai <shyokmg.au@gmail.com>
2023-12-31 17:23:59 +1100

    Added basic score and start button

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -6,6 +6,7 @@ import Fruit from './fruit';
 const canvas = document.querySelector('canvas');
 const c = canvas.getContext('2d');
 const gravity = 0.5;
+const button = document.querySelector('button');
 
 canvas.width = 1024;
 canvas.height = 576;
@@ -13,8 +14,11 @@ export { canvas, c, gravity };
 
 let player = new Player()
 let floor = new Floor();
-
+let hearts;
+let score;
+let fruitCounter;
 let lastKey
+let fruits = []
 
 const keys = {
   right: {
@@ -26,41 +30,55 @@ const keys = {
 }
 
 function init() {
+  // initialize
   player = new Player();
   floor = new Floor();
+  hearts = 10;
+  score = 0;
+  fruits = [];
+  fruitCounter = 30
+  spawnFruit(fruitCounter);
 }
 
-let fruits = []
 
-function spawnFruit(fruitCount) {
-  for (let i = 1; i < fruitCount + 1; i++) {
-    const yOffset = i * (-500);
-    let xPosition = Math.floor(Math.random() * 1000);
-    fruits.push(
-      new Fruit({ position: { x: xPosition, y: yOffset } })
-    )
-  }
-}
-
-spawnFruit(30);
-
-function animate() {
-  requestAnimationFrame(animate)
+function update() {
+  requestAnimationFrame(update)
   c.fillStyle = 'white'
   c.fillRect(0, 0, canvas.width, canvas.height);
 
   floor.draw();
+  // console.log(hearts);
   player.update();
 
-  fruits.forEach(fruit => {
+  fruits.forEach((fruit, i) => {
     fruit.update();
     if (boxCollision(fruit, floor)) {
-      fruit.velocity.y = 0;
+      // fruit.velocity.y = 0;
+      fruits.splice(i, 1);
+      --fruitCounter;
+      --hearts;
+    }
+    if (boxCollision(fruit, player)) {
+      fruits.splice(i, 1);
+      --fruitCounter;
+      ++score;
     }
   });
 
-  console.log(fruits[0].position.y);
+  console.log(score);
 
+  if (hearts <= 0) {
+    console.log(`Game over, your score is ${score}`);
+    // init();
+    fruits = [];
+    button.style.display = 'block';
+  } else if (fruitCounter <= 0) {
+    console.log(`Game over, your score is ${score}`)
+    // init();
+    button.style.display = 'block';
+  }
+
+  
 
   // If right or left keys are pressed move right or left in 5px
   if (keys.right.pressed && player.position.x < 977) {
@@ -78,6 +96,21 @@ function animate() {
   }
 }
 
+update();
+
+function spawnFruit(fruitCount) {
+  for (let i = 1; i < fruitCount + 1; i++) {
+    const yOffset = i * (-500);
+    let xPosition = Math.floor(Math.random() * 1000);
+    fruits.push(
+      new Fruit({ position: { x: xPosition, y: yOffset } })
+    )
+  }
+}
+
+// spawnFruit(30);
+
+
 // collision detection
 function boxCollision(obj1, obj2) {
   if (obj1.position.y + obj1.height <= obj2.position.y &&
@@ -87,9 +120,6 @@ function boxCollision(obj1, obj2) {
     return true;
   }
 }
-
-init();
-animate();
 
 // Event listener for pressing down on a key 
 addEventListener('keydown', ({ keyCode }) => {
@@ -132,4 +162,10 @@ addEventListener('keyup', ({ keyCode }) => {
       break;
   }
 });
+
+button.addEventListener('click', (event) => {
+  button.style.display = 'none';
+  init();
+  // spawnFruit(30);
+})
 
